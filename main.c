594042d11f43ae35a7e2e124dd1a16892ea0d404
albakourhend-a11y <albakourhend@gmail.com>
2025-09-28 12:12:25 +0300

    updated

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 # include "headers.h"
+
+int** board = createGrid();
+int row;
 int column;
-char currentPlayer; 
-printf("Player %c, choose a column (0-7): ", currentPlayer);
-        scanf("%d", &column);
+
+printf("Welcome to Connect Four!");
+
+char playerA = 'A';
+char playerB = 'B';
+printf("Player A: %c\n", playerA);
+printf("Player B: %c\n\n", playerB);
+
+print(board);
+
+printf("Player %c, choose a column (0-7): ", playerA);
+ scanf("%d", &column);
+
+
