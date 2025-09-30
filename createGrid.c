ff428333a329d60b06e94af2061a00ca0a48bc21
Striver29 <aha150@mail.aub.edu>
2025-10-01 01:45:25 +0300

    Print function Implemented

--- createGrid.c
+++ createGrid.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "headers.h"
 
-void createGrid(){
+int** createGrid(){
     int n = 7, m = 6;
     int** grid = (int**)malloc(n * sizeof(int*));
     for(int i = 0; i < n; i++){
-        grid[i] = (int)malloc(m * sizeof(int));
+        grid[i] = (int*)malloc(m * sizeof(int));
     }
+
+    return grid; 
     
 }
 
--- print.c
+++ print.c
@@ -3,5 +3,22 @@
 
 void print(int** arr) {
     
+    for(int i = 0; i < 6; i++) {
+        for(int j = 0; j < 7; j++) {
+            if(arr[i][j] == 0) {
+                printf(". ");
+            } else if(arr[i][j] == 1) {
+                printf("A "); 
+            } else if(arr[i][j] == 2) {
+                printf("B "); 
+            }
+        }
+
+        printf("\n");
+    }
+
+    for(int i = 1; i < 8; i++) {
+        printf("%d ", i); 
+    }
 }
  
