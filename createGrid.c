822a2faa904765f1806f0236dbf795351f1a83c1
Moe <mia70@mail.aub.edu>
2025-10-01 01:31:24 +0300

    Create Grid

--- createGrid.c
+++ createGrid.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include "headers.h"
 
+void createGrid(){
+    int n = 7, m = 6;
+    int** grid = (int**)malloc(n * sizeof(int*));
+    for(int i = 0; i < n; i++){
+        grid[i] = (int)malloc(m * sizeof(int));
+    }
+    
+}
+
 
 
