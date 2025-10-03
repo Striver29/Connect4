4855d715a0f99f2733744880c863a88a618eafd4
albakourhend-a11y <albakourhend@gmail.com>
2025-10-03 21:55:16 +0300

    main fixed

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+# include "headers.h"
+
+int row;
+int column;
+
+void main(){
+    
+    
+int** board = createGrid();
+printf("Welcome to Connect Four!");
+
+char playerA = 'A';
+char playerB = 'B';
+char currentPlayer= playerA;
+printf("Player A: %c\n", playerA);
+printf("Player B: %c\n\n", playerB);
+
+while (true){ 
+// print empty  broad first
+print(board);
+
+
+printf("Player %c, choose a column (0-7): ", playerA);
+ scanf("%d", &column);
+
+// eza msh value akbar mn column 
+if (column < 0 || column >=7){
+    Printf("Invalid! choose another.\n");
+         continue;
+}
+
+
+// eza fe place 
+if (!place(board, column, currentPlayer)) {
+      printf("Column full! Choose another.\n");
+         continue; 
+        }
+
+
+// switch players
+ if (currentPlayer == playerA) {
+            currentPlayer = playerB;
+        } else {
+            currentPlayer = playerA;
+        }
+}
+printf("Player %c wins!", currentPlayer);
+printf("Program finished with exit code 0");
+}
