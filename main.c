7fd370405923f227469a29465bb24cee4e742609
albakourhend-a11y <albakourhend@gmail.com>
2025-10-15 01:45:33 +0300

    Main Updated

--- main.c
+++ main.c
@@ -7,14 +7,23 @@ int column;
 
 int main(int argc, char** argv){
     
-    
 int** board = createGrid();
 printf("Welcome to Connect Four!\n");
+
+printf("Do you want to play against human or bot? \n Enter 0 for human or 1 for bot: ");
+int x;
+scanf("%d",&x);
+
+int mode = 0; 
+if(x==1){
+printf("Choose the level: \n Enter 1 for easy, 2 for Medium, 3 for Hard:  ");
+scanf("%d",&mode);
+}
 
 char playerA = 'A';
 char playerB = 'B';
 char currentPlayer= playerA;
-printf("Player A: %c\n", playerA);
+printf("\nPlayer A: %c\n", playerA);
 printf("Player B: %c\n\n", playerB);
 
 while (1){ 
@@ -24,12 +33,14 @@ print(board);
 
 printf("Player %c, choose a column (1-7): ", currentPlayer);
 fflush(stdout);
-scanf("%d", &column);
 
- if(column == 10) {
-    break; 
- }
-
+if(currentPlayer == playerB) {
+    if(mode == 1) {
+        column = easyBot(board); 
+    } else {
+        scanf("%d", &column);
+    }
+}
 
 if (column < 1 || column >7){
     printf("\nInvalid! choose another.\n");
