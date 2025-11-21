6a33850c5406015ced2909d05b5743c649202919
Striver29 <aha150@mail.aub.edu>
2025-11-22 01:16:30 +0200

    Helpers

--- hardBot.c
+++ hardBot.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 // check move
 int checkMove(int** arr, int column){
   if(column >7 && column<0){
@@ -8,7 +10,29 @@ int checkMove(int** arr, int column){
 return 0; 
 }
 
+int checkFull(int** arr) {
+    for(int i = 0; i < 7; i++) {
+        if(arr[0][i] == 0) {
+            return 0; 
+        }
+    }
 
+    return 1; 
+}
+
+int LandingRow(int** arr, int col) {
+    for(int i = 5; i >= 0; i--) {
+        if(arr[i][col] == 0) {
+            return i; 
+        }
+    }
+
+    return -1; 
+}
+
+void simulatePlace(int** arr, int row, int col, int player) {
+    arr[row][col] = player; 
+}
 
 // undoMove
 void undoMove(int** arr, int row , int col){
