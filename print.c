96f12978fbe849528e97294e2797c473f962cdc1
Striver29 <aha150@mail.aub.edu>
2025-11-23 18:04:51 +0200

    additions

--- print.c
+++ print.c
@@ -34,28 +34,25 @@ void print(int **arr, char type)
     }
     else
     {
-        for (int i = 0; i < 6; i++)
-        {
-            printf(" ");
-            for (int j = 0; j < 7; j++)
-            {
-
-                if (arr[i][j] == 0)
-                {
-                    printf(". "); // empty
-                }
-                else if (arr[i][j] == 1)
-                {
-                    printf("A "); // A
-                }
-                else if (arr[i][j] == 2)
-                {
-                    printf("B "); // B
-                }
+           for(int i = 0; i < 6; i++) {
+        for(int j = 0; j < 7; j++) {
+            if(arr[i][j] == 0) {
+                printf(". ");
+            } else if(arr[i][j] == 1) {
+                printf("A "); 
+            } else if(arr[i][j] == 2) {
+                printf("B "); 
             }
-            printf("\n");
         }
 
-        printf(" 1 2 3 4 5 6 7\n\n");
+        printf("\n");
+    }
+
+    for(int i = 1; i < 8; i++) {
+        printf("%d ", i); 
+    }
+    
+    printf("\n");
+
     }
 }
